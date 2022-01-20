8c56e87d91da2ce13e48f6b6ba055978ae6eb7fe
Isaac Ren <isaac.ren@inria.fr>
2022-01-20 17:54:38 +0100

    Added more explanation

--- guess_wordle.py
+++ guess_wordle.py
@@ -1,20 +1,24 @@
+import sys
+
 if __name__ == "__main__":
 	print("Processing guess results...")
 	
+	# Read words and all possible guess results
 	words_file = open("words.csv", 'r')
-	guesses_file = open("guesses.csv", 'r')
-	
 	words = []
 	for line in words_file:
 		guess_results = line.rstrip(' ,\n').split(',')
 		solution = guess_results.pop(0)
-		words.append([solution, guess_results])
+		words.append((solution, guess_results))
 	
+	# Read others' guess results
+	guesses_file = open(sys.argv[1], 'r')
 	guesses = []
 	for line in guesses_file:
 		guesses.extend(line.rstrip(' ,\n').replace(' ','').split(','))
 	guesses = set(guesses)
 	
+	# Get and return candidates
 	candidates = []
 	for word, guess_results in words:
 		if guesses.issubset(guess_results):
